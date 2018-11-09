31bcbb88c7d2882332f869517170b6157c94d812
Vodash <k.udema@st.hanze.nl>
2018-11-09 13:10:38 +0100

    Scheduler veranderd naar de volledige versie van de blackboard scheduler
    Scheduler zo veranderd dat er nu gebruik wordt gemaakt van de scheduler  die gegeven is op blackboard.

--- C/scheduler.c
+++ C/scheduler.c
@@ -1,192 +1,223 @@
-/*
- * GccApplication5.c
- *
- * Created: 5-11-2018 16:31:20
- *  Author: Maarten
- */ 
-
-
+#include "scheduler.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include "scheduler.h"
 
 // The array of tasks
-Task scheduler_tasks_g[SCHEDULER_MAX_TASKS];
+sTask SCH_tasks_G[SCH_MAX_TASKS];
+
 
 /*------------------------------------------------------------------*-
-  scheduler_dispatch_tasks()
+
+  SCH_Dispatch_Tasks()
+
   This is the 'dispatcher' function.  When a task (function)
-  is due to run, scheduler_dispatch_tasks() will run it.
+  is due to run, SCH_Dispatch_Tasks() will run it.
   This function must be called (repeatedly) from the main loop.
+
 -*------------------------------------------------------------------*/
 
-void scheduler_dispatch_tasks(void)
+void SCH_Dispatch_Tasks(void)
 {
-    // Dispatches (runs) the next task (if one is ready)
-    for (unsigned char index = 0; index < SCHEDULER_MAX_TASKS; index++)
-    {
-        if ((scheduler_tasks_g[index].runme > 0) && (scheduler_tasks_g[index].task_pointer != 0))
-        {
-            (*scheduler_tasks_g[index].task_pointer)();  // Run the task
-            scheduler_tasks_g[index].runme--;   // Reset / reduce runme flag
+   unsigned char Index;
 
-            // Periodic tasks will automatically run again
-            // - if this is a 'one shot' task, remove it from the array
-            if (scheduler_tasks_g[index].period == 0)
-            {
-                scheduler_delete_task(index);
-            }
-        }
-    }
+   // Dispatches (runs) the next task (if one is ready)
+   for(Index = 0; Index < SCH_MAX_TASKS; Index++)
+   {
+      if((SCH_tasks_G[Index].RunMe > 0) && (SCH_tasks_G[Index].pTask != 0))
+      {
+         (*SCH_tasks_G[Index].pTask)();  // Run the task
+         SCH_tasks_G[Index].RunMe -= 1;   // Reset / reduce RunMe flag
+
+         // Periodic tasks will automatically run again
+         // - if this is a 'one shot' task, remove it from the array
+         if(SCH_tasks_G[Index].Period == 0)
+         {
+            SCH_Delete_Task(Index);
+         }
+      }
+   }
 }
 
 /*------------------------------------------------------------------*-
-  scheduler_add_task()
+
+  SCH_Add_Task()
+
   Causes a task (function) to be executed at regular intervals 
   or after a user-defined delay
+
   pFunction - The name of the function which is to be scheduled.
               NOTE: All scheduled functions must be 'void, void' -
               that is, they must take no parameters, and have 
               a void return type. 
                    
   DELAY     - The interval (TICKS) before the task is first executed
+
   PERIOD    - If 'PERIOD' is 0, the function is only called once,
               at the time determined by 'DELAY'.  If PERIOD is non-zero,
               then the function is called repeatedly at an interval
               determined by the value of PERIOD (see below for examples
               which should help clarify this).
+
+
   RETURN VALUE:  
+
   Returns the position in the task array at which the task has been 
-  added.  If the return value is SCHEDULER_MAX_TASKS then the task could 
+  added.  If the return value is SCH_MAX_TASKS then the task could 
   not be added to the array (there was insufficient space).  If the
-  return value is < SCHEDULER_MAX_TASKS, then the task was added 
+  return value is < SCH_MAX_TASKS, then the task was added 
   successfully.  
+
   Note: this return value may be required, if a task is
-  to be subsequently deleted - see scheduler_delete_task().
+  to be subsequently deleted - see SCH_Delete_Task().
+
   EXAMPLES:
-  Task_ID = scheduler_add_task(Do_X,1000,0);
+
+  Task_ID = SCH_Add_Task(Do_X,1000,0);
   Causes the function Do_X() to be executed once after 1000 sch ticks.            
-  Task_ID = scheduler_add_task(Do_X,0,1000);
+
+  Task_ID = SCH_Add_Task(Do_X,0,1000);
   Causes the function Do_X() to be executed regularly, every 1000 sch ticks.            
-  Task_ID = scheduler_add_task(Do_X,300,1000);
+
+  Task_ID = SCH_Add_Task(Do_X,300,1000);
   Causes the function Do_X() to be executed regularly, every 1000 ticks.
   Task will be first executed at T = 300 ticks, then 1300, 2300, etc.            
  
 -*------------------------------------------------------------------*/
 
-unsigned char scheduler_add_task(void (*pFunction)(), const unsigned int DELAY, const unsigned int PERIOD)
+unsigned char SCH_Add_Task(void (*pFunction)(), const unsigned int DELAY, const unsigned int PERIOD)
 {
-    unsigned char index = 0;
+   unsigned char Index = 0;
 
-    // First find a gap in the array (if there is one)
-    while ((scheduler_tasks_g[index].task_pointer != 0) && (index < SCHEDULER_MAX_TASKS))
-    {
-        index++;
-    }
+   // First find a gap in the array (if there is one)
+   while((SCH_tasks_G[Index].pTask != 0) && (Index < SCH_MAX_TASKS))
+   {
+      Index++;
+   }
 
-    // Have we reached the end of the list?
-    if (index == SCHEDULER_MAX_TASKS)
-    {
-        // Task list is full, return an error code
-        return SCHEDULER_MAX_TASKS;  
-    }
+   // Have we reached the end of the list?   
+   if(Index == SCH_MAX_TASKS)
+   {
+      // Task list is full, return an error code
+      return SCH_MAX_TASKS;  
+   }
 
-    // If we're here, there is a space in the task array
-    scheduler_tasks_g[index].task_pointer = pFunction;
-    scheduler_tasks_g[index].delay = DELAY;
-    scheduler_tasks_g[index].period = PERIOD;
-    scheduler_tasks_g[index].runme = 0;
+   // If we're here, there is a space in the task array
+   SCH_tasks_G[Index].pTask = pFunction;
+   SCH_tasks_G[Index].Delay =DELAY;
+   SCH_tasks_G[Index].Period = PERIOD;
+   SCH_tasks_G[Index].RunMe = 0;
 
-    // return position of task (to allow later deletion)
-    return index;
+   // return position of task (to allow later deletion)
+   return Index;
 }
 
 /*------------------------------------------------------------------*-
-  scheduler_delete_task()
+
+  SCH_Delete_Task()
+
   Removes a task from the scheduler.  Note that this does
   *not* delete the associated function from memory: 
   it simply means that it is no longer called by the scheduler. 
  
-  TASK_INDEX - The task index.  Provided by scheduler_add_task(). 
+  TASK_INDEX - The task index.  Provided by SCH_Add_Task(). 
+
   RETURN VALUE:  RETURN_ERROR or RETURN_NORMAL
+
 -*------------------------------------------------------------------*/
 
-unsigned char scheduler_delete_task(const unsigned char TASK_INDEX)
+unsigned char SCH_Delete_Task(const unsigned char TASK_INDEX)
 {
-    scheduler_tasks_g[TASK_INDEX].task_pointer = 0;
-    scheduler_tasks_g[TASK_INDEX].delay = 0;
-    scheduler_tasks_g[TASK_INDEX].period = 0;
-    scheduler_tasks_g[TASK_INDEX].runme = 0;
+   // Return_code can be used for error reporting, NOT USED HERE THOUGH!
+   unsigned char Return_code = 0;
 
-    return (unsigned char) 0;
+   SCH_tasks_G[TASK_INDEX].pTask = 0;
+   SCH_tasks_G[TASK_INDEX].Delay = 0;
+   SCH_tasks_G[TASK_INDEX].Period = 0;
+   SCH_tasks_G[TASK_INDEX].RunMe = 0;
+
+   return Return_code;
 }
 
 /*------------------------------------------------------------------*-
-  scheduler_init_t1()
+
+  SCH_Init_T1()
+
   Scheduler initialisation function.  Prepares scheduler
   data structures and sets up timer interrupts at required rate.
   You must call this function before using the scheduler.  
+
 -*------------------------------------------------------------------*/
 
-void scheduler_init_t1(void)
+void SCH_Init_T1(void)
 {
-    for (unsigned char i = 0; i < SCHEDULER_MAX_TASKS; i++)
-    {
-        scheduler_delete_task(i);
-    }
+   unsigned char i;
 
-    // Set up Timer 1
-    // Values for 1ms and 10ms ticks are provided for various crystals
+   for(i = 0; i < SCH_MAX_TASKS; i++)
+   {
+      SCH_Delete_Task(i);
+   }
 
-    OCR1A = (uint16_t) 625;                  // 10ms = (256/16.000.000) * 625
-    TCCR1B = (1 << CS12) | (1 << WGM12);     // prescale op 64, top counter = value OCR1A (CTC mode)
-    TIMSK1 = 1 << OCIE1A;                    // Timer 1 Output Compare A Match Interrupt Enable
+   // Set up Timer 1
+   // Values for 1ms and 10ms ticks are provided for various crystals
+
+   // Hier moet de timer periode worden aangepast ....!
+   OCR1A = (uint16_t)625;   		     // 10ms = (256/16.000.000) * 625
+   TCCR1B = (1 << CS12) | (1 << WGM12);  // prescale op 64, top counter = value OCR1A (CTC mode)
+   TIMSK1 = 1 << OCIE1A;   		     // Timer 1 Output Compare A Match Interrupt Enable
 }
 
 /*------------------------------------------------------------------*-
-  scheduler_start()
+
+  SCH_Start()
+
   Starts the scheduler, by enabling interrupts.
+
   NOTE: Usually called after all regular tasks are added,
   to keep the tasks synchronised.
+
   NOTE: ONLY THE SCHEDULER INTERRUPT SHOULD BE ENABLED!!! 
  
 -*------------------------------------------------------------------*/
 
-void scheduler_start(void)
+void SCH_Start(void)
 {
-    sei();
+      sei();
 }
 
 /*------------------------------------------------------------------*-
-  SCHEDULER_Update
+
+  SCH_Update
+
   This is the scheduler ISR.  It is called at a rate 
-  determined by the timer settings in scheduler_init_t1().
+  determined by the timer settings in SCH_Init_T1().
+
 -*------------------------------------------------------------------*/
 
 ISR(TIMER1_COMPA_vect)
 {
-    for (unsigned char index = 0; index < SCHEDULER_MAX_TASKS; index++)
-    {
-        // Check if there is a task at this location
-        if (scheduler_tasks_g[index].task_pointer)
-        {
-            if (scheduler_tasks_g[index].delay == 0)
-            {
-                // The task is due to run, Inc. the 'RunMe' flag
-                scheduler_tasks_g[index].runme++;
+   unsigned char Index;
+   for(Index = 0; Index < SCH_MAX_TASKS; Index++)
+   {
+      // Check if there is a task at this location
+      if(SCH_tasks_G[Index].pTask)
+      {
+         if(SCH_tasks_G[Index].Delay == 0)
+         {
+            // The task is due to run, Inc. the 'RunMe' flag
+            SCH_tasks_G[Index].RunMe += 1;
 
-                if (scheduler_tasks_g[index].period)
-                {
-                    // Schedule periodic tasks to run again
-                    scheduler_tasks_g[index].delay = scheduler_tasks_g[index].period;
-                    scheduler_tasks_g[index].delay--;
-                }
-            }
-            else
+            if(SCH_tasks_G[Index].Period)
             {
-                // Not yet ready to run: just decrement the delay
-                scheduler_tasks_g[index].delay--;
+               // Schedule periodic tasks to run again
+               SCH_tasks_G[Index].Delay = SCH_tasks_G[Index].Period;
+               SCH_tasks_G[Index].Delay -= 1;
             }
-        }
-    }
+         }
+         else
+         {
+            // Not yet ready to run: just decrement the delay
+            SCH_tasks_G[Index].Delay -= 1;
+         }
+      }
+   }
 }
--- C/scheduler.h
+++ C/scheduler.h
@@ -1,28 +1,27 @@
-#ifndef _scheduler
-#define _scheduler
-
 // Scheduler data structure for storing task data
 typedef struct
 {
 	// Pointer to task
-	void (*task_pointer)(void);
+	void (* pTask)(void);
 	// Initial delay in ticks
-	unsigned int delay;
+	unsigned int Delay;
 	// Periodic interval in ticks
-	unsigned int period;
+	unsigned int Period;
 	// Runme flag (indicating when the task is due to run)
-	unsigned char runme;
-} Task;
+	unsigned char RunMe;
+} sTask;
 
 // Function prototypes
-void scheduler_init_t1(void);
-void scheduler_start(void);
+//-------------------------------------------------------------------
 
+void SCH_Init_T1(void);
+void SCH_Start(void);
 // Core scheduler functions
-void scheduler_dispatch_tasks(void);
-unsigned char scheduler_add_task(void (*)(void), const unsigned int, const unsigned int);
-unsigned char scheduler_delete_task(const unsigned char);
+void SCH_Dispatch_Tasks(void);
+unsigned char SCH_Add_Task(void (*)(void), const unsigned int, const unsigned int);
+unsigned char SCH_Delete_Task(const unsigned char);
 
-#define SCHEDULER_MAX_TASKS 5
+// hier het aantal taken aanpassen ....!!
+// Maximum number of tasks
 
-#endif
+#define SCH_MAX_TASKS (5)
